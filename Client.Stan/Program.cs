68ccc1e40e56054c6e1b422348aabb16437b123a
Serega800 <svs3107@gmail.com>
2022-02-06 21:24:08 +0500

    Добавлен клиент Стэна

---
+++ Client.Stan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client.Stan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
